a69728f3dc2e7ca052376909e8e24d6f7c1d757a
GunnerJnr <gunnerjnr@live.co.uk>
2018-06-07 11:10:12 +0100

    updated year to 2049 so timer will run for a good while

--- js/countdown.js
+++ js/countdown.js
@@ -1,8 +1,8 @@
 var countdownClock;
 var breakdownClock;
 // Set the date we're counting down to
-var countdownDate = new Date("June 7, 2018 11:00:00"); //Month Days, Year HH:MM:SS
-var breakdownDate = new Date("June 7, 2018 17:30:00"); //Month Days, Year HH:MM:SS
+var countdownDate = new Date("June 7, 2049 11:00:00"); //Month Days, Year HH:MM:SS
+var breakdownDate = new Date("June 7, 2049 17:30:00"); //Month Days, Year HH:MM:SS
 // Get todays date and time
 var now = new Date();
 // Find the distance between now an the count down date
